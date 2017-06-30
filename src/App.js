b0e3a1ddd8343ee35eb624572013e80943ff6e34
Sherwin Mina <sherwinmina@yahoo.com>
2017-06-30 13:47:09 -0700

    updating the to check for winner

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +8,9 @@ class App extends Component {
     this.state = {
       turn: 'X',
       gameEnded: false,
-      board: Array(9).fill('')
+      board: Array(9).fill(''),
+      totalMoves: 0,
+      winner: undefined
     }
   }
   
@@ -19,16 +20,54 @@ class App extends Component {
       event.target.innerText = this.state.turn;
       this.setState({
         turn: this.state.turn == 'X' ? 'O' : 'X',
-        board: this.state.board
+        board: this.state.board,
+        totalMoves: this.state.totalMoves++
       });
 
-      console.log(this.state.board);
+      var result = this.checkWinner();
+
+      if( result == 'X') {
+        this.setState({
+          gameEnded: true,
+          winner: 'X',
+          winnerLine: 'X won the game'
+        });
+      } else if ( result == 'O') {
+        this.setState({
+          gameEnded: true,
+          winner: 'O',
+          winnerLine: 'O won the game'
+        });
+      } else if (result == 'draw') {
+        this.setState({
+          gameEnded: true,
+          winner: 'draw',
+          winnerLine: 'The match is a draw'
+        })
+      }
+      console.log(this.state.gameEnded);
     }
+  }
+
+  checkWinner() {
+    // var moves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2]];
+    var { board } = this.state;
+    for(let i=0;i<moves.length;i++) {
+      if(board[moves[i][0]] == board[moves[i][1]] && board[moves[i][1]] == board[moves[i][2]])
+          return board[moves[i][0]];
+    }
+
+    if (this.state.totalMoves == 9) {
+      return 'draw';
+    }
+
   }
 
   render() {
     return (
       <div className="game">
+      <div className="status">{this.state.winnerLine}</div>
         <div className="head">
           <h1>Tic-Tac-Toe</h1>
         </div>
