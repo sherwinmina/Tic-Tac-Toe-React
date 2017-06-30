437dfd2404378cc1e792a8eec2d6c5c3872a20b1
Sherwin Mina <sherwinmina@yahoo.com>
2017-06-30 12:35:06 -0700

    basic grid layout for the game

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="game">
+        <div className="head">
+          <h1>Tic-Tac-Toe</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="board">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+        </div>
       </div>
     );
   }
